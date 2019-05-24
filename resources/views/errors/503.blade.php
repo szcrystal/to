5879f65a6e4472c3b4ced35cd4c4badf0bdaed9a
szcrystal <szk.create@gmail.com>
2019-05-24 10:11:32 +0000

    to

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,12 @@
+@extends('errors::minimal')
+
+@section('title', __('_'))
+@section('code', '')
+@section('message', __($exception->getMessage() ?: ''))
+
+
+{{--
+@section('title', __('Service Unavailable'))
+@section('code', '503')
+@section('message', __($exception->getMessage() ?: 'Service Unavailable'))
+--}}
